df9635ed0efa90827cf5ea16c0e3b534c3b425b9
Talita <talita.anthonio@yahoo.com>
2021-06-02 16:01:20 +0200

    script to make embeddings

---
+++ word-embeddings/make_embeddings.py
@@ -0,0 +1,45 @@
+import json 
+
+
+path_to_pred_dir = "/Users/talita/Documents/PhD/tacl/analyse-predictions" 
+path_to_file_with_predictions = '{0}/bestmodels_predictions.json'.format(path_to_pred_dir)
+path_to_filtered_fillers = "../coreference/dev_set_with_filtered_fillers.json"
+
+with open(path_to_file_with_predictions, "r") as json_in: 
+     data = json.load(json_in)
+
+
+with open(path_to_filtered_fillers, "r") as json_in: 
+     filtered_fillers = json.load(json_in)
+
+
+class RevisionInstance: 
+
+    # revision_instance = data[key]
+    def __init__(self, key, revision_instance, keys):
+        self.revision_instance = revision_instance
+        self.key = key 
+        self.keys = keys 
+        self.left_context = revision_instance["LeftContext"]
+        self.predictions = [prediction.strip() for prediction in revision_instance["GPT+FinetuningPred"]]
+        self.filtered_fillers = filtered_fillers[key]["filtered_fillers"]
+        self.revised_after_insertion = revision_instance["revised_after_insertion"]
+
+    @property 
+    def revised_untill_insertion(self): 
+        if "revised_untill_insertion" in self.keys: 
+           return self.revision_instance["revised_untill_insertion"]
+        else: 
+           return self.revision_instance["revised_until_insertion"]
+
+
+
+
+def main(): 
+    for key, _ in data.items(): 
+        revision_object = RevisionInstance(key, data[key], data[key].keys())
+        print(revision_object.filtered_fillers)
+        break 
+
+
+main()
