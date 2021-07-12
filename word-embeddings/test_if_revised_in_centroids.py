52aeb7cce891c221d061022b2b5e3885b5f429f5
Talita <talita.anthonio@yahoo.com>
2021-07-12 20:51:27 +0200

    test if revised in centroids.

--- word-embeddings/test_if_revised_in_centroids.py
+++ word-embeddings/test_if_revised_in_centroids.py
@@ -2,9 +2,9 @@ import json
 import pdb 
 
 
-PATH_TO_FILE = "../coreference/filtered_dev_preds_final.json"
+PATH_TO_FILE = "../coreference/filtered_train_preds_final.json"
 NUM_OF_PRED = 20 
-PATH_TO_CLUSTERS = "kmeans_k=5_filtered_step1_top{0}_with_rev_v2.json".format(NUM_OF_PRED)
+PATH_TO_CLUSTERS = "kmeans_k=5_train.json".format(NUM_OF_PRED)
 
 with open(PATH_TO_CLUSTERS, "r") as json_in: 
      clusters = json.load(json_in)
@@ -18,12 +18,19 @@ for key, _ in data.items():
     preds = clusters[key]["Centroids_with_revised"]
     print(preds)
 
-    revised_sentence = data[key]["RevisedSentence"]
+    revised_sentence = data[key]["revised_sentence"]
     if "revised_untill_insertion" in data[key].keys(): 
         revised_untill_insertion = data[key]["revised_untill_insertion"]
     else: 
         revised_untill_insertion = data[key]["revised_until_insertion"]
-    revised_sentence_alt = revised_untill_insertion + " " + data[key]["CorrectReference"] + " " + data[key]["revised_after_insertion"]  
+    
+    reference = " ".join(data[key]["reference"])
+
+    if 'revised_after_insertion' in data[key].keys(): 
+        revised_after_insertion = data[key]["revised_after_insertion"]
+    else: 
+        revised_after_insertion = data[key]["revised_afer_insertion"]
+    revised_sentence_alt = revised_untill_insertion + " " + reference + " " + revised_after_insertion
     
     if revised_sentence_alt or revised_sentence in preds: 
        print('correct')
