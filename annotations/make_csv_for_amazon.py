6283cb87d9a7d1fd8ad869ca8eebb45ae9fa6ff5
Talita <talita.anthonio@yahoo.com>
2021-07-19 21:35:01 +0200

    stage current.

--- annotations/make_csv_for_amazon.py
+++ annotations/make_csv_for_amazon.py
@@ -10,8 +10,11 @@ from collections import Counter
 import numpy as np 
 
 PATH_TO_FILE = "../coreference/filtered_dev_preds_final.json"
+PATH_TO_TRAIN_FILE = "../coreference/filtered_train_preds_final.json"
+
 NUM_OF_PRED = 20
 PATH_TO_CLUSTERS = "../word-embeddings/kmeans_k=5_filtered_step1_top{0}_with_rev_v2.json".format(NUM_OF_PRED)
+PATH_TO_CLUSTERS_TRAIN = "../word-embeddings/kmeans_k=5_train.json"
 PATH_TO_FILE_OUT = "./amazon_file.csv"
 
 with open(PATH_TO_CLUSTERS, "r") as json_in: 
@@ -20,6 +23,17 @@ with open(PATH_TO_CLUSTERS, "r") as json_in:
 
 with open(PATH_TO_FILE, "r") as json_in: 
      data = json.load(json_in)
+
+
+with open(PATH_TO_TRAIN_FILE, "r") as json_in: 
+     train_data = json.load(json_in)
+
+with open(PATH_TO_CLUSTERS_TRAIN, "r") as json_in: 
+     train_data_clusters = json.load(json_in)
+
+
+data.update(train_data)
+clusters.update(train_data_clusters)
 
 
 with open("../../tacl/data/references_for_lm.json", "r") as json_in: 
@@ -45,29 +59,31 @@ def main():
     average_length = []
     for key, _ in data.items(): 
         if len(clusters[key]["Centroids_with_revised"]) == 5: 
+            print("TITLE", format_text(all_data[key]["filename"]))
             try: 
                 print(all_data[key]["Base_Article_Clean"]["left"])
                 print("========= currrent ====") 
                 print(all_data[key]["Base_Article_Clean"]["current"])
 
+            
+            except KeyError: 
+
+   
                 print("========= par ===============")
                 print(all_data[key]["par"])
                 print("ref", all_data[key]["reference"])
                 print("revised sent", all_data[key]["revised_sentence"])
-                print("predictions", data[key]["GPT+Finetuning+P-perplexityPred"])
+                print("filtered predictions", data[key]["filtered_fillers"])
+                print("clusters", clusters[key]["Centroids_with_revised"])
+                #print("predictions", data[key]["GPT+Finetuning+P-perplexityPred"])
                 
-            except KeyError: 
-                print("not found")
-                print(all_data[key]["par"])
-            
 
-            print(all_data[key]["par"])
-            d["Title"].append(format_text(all_data[key]["filename"]))
-            formatted = " ".join(trunc_par(data[key]["par"])).strip('\n')
-            average_length.append(len(data[key]["par"].strip('\n').split('\n')))
-            d["Context"].append(formatted.replace('\n', "<br>"))
-            d["Sent"].append(data[key]["RevisedSentence"])
-            d["Reference"].append(all_data[key]["reference"])
+            #d["Title"].append(format_text(all_data[key]["filename"]))
+            #formatted = " ".join(trunc_par(data[key]["par"])).strip('\n')
+            #average_length.append(len(data[key]["par"].strip('\n').split('\n')))
+            #d["Context"].append(formatted.replace('\n', "<br>"))
+            #d["Sent"].append(data[key]["RevisedSentence"])
+            #d["Reference"].append(all_data[key]["reference"])
             
 
         print("==============")
