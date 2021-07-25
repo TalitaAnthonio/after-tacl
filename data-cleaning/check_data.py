a19e3d12ea45dc464d374cafa822cf5868349304
Talita <talita.anthonio@yahoo.com>
2021-07-25 13:20:49 +0200

    add script to check data.

--- data-cleaning/check_data.py
+++ data-cleaning/check_data.py
@@ -1,2 +1,30 @@
 import json 
+import pdb 
+from collections import Counter 
+import spacy 
 
+ # 1428, 'you': 1040,
+spacy_model = spacy.load("en_core_web_sm")
+
+
+def get_pos_tags(filler): 
+    parsed = spacy_model(filler)
+    return [token.tag_ for token in parsed]
+
+with open("../../tacl/data/references_for_lm.json", "r") as json_in: 
+     all_data = json.load(json_in)
+
+# count how many references there are 
+freq_dict = Counter()
+references = [" ".join(all_data[key]['reference']).lower() if type(all_data[key]["reference"]) == list else all_data[key]['reference'] for key, _ in all_data.items()]
+
+# exclude references 
+for key, _ in all_data.items(): 
+    if type(all_data[key]["reference"]) == list: 
+        reference = " ".join(all_data[key]['reference']) 
+    else: 
+        reference = all_data[key]["reference"]
+    tagged_reference = get_pos_tags(reference)
+
+    if tagged_reference != ['PRP'] and tagged_reference != ['PRP$']: 
+        print(tagged_reference, reference)
