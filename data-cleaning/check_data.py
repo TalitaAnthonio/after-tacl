f2420e1afec12f7d0fcc1f6518c9400780179688
Talita <talita.anthonio@yahoo.com>
2021-07-27 16:16:16 +0200

    check the data.

--- data-cleaning/check_data.py
+++ data-cleaning/check_data.py
@@ -7,6 +7,14 @@ import spacy
 spacy_model = spacy.load("en_core_web_sm")
 
 
+# file with predictions 
+PATH_TO_DEV_FILE_PRED = "../coreference/filtered_dev_preds_final.json"
+PATH_TO_TRAIN_FILE_PRED = "../coreference/filtered_train_preds_final.json"
+
+
+
+
+
 def get_pos_tags(filler): 
     parsed = spacy_model(filler)
     return [token.tag_ for token in parsed]
@@ -14,12 +22,22 @@ def get_pos_tags(filler):
 with open("../../tacl/data/references_for_lm.json", "r") as json_in: 
      all_data = json.load(json_in)
 
+
+with open(PATH_TO_DEV_FILE_PRED, "r") as json_in: 
+     predictions_data = json.load(json_in)
+
+with open(PATH_TO_TRAIN_FILE_PRED, "r") as json_in: 
+     predictions_train = json.load(json_in)
+
+predictions_data.update(predictions_train)
+
+
 # count how many references there are 
 freq_dict = Counter()
 references = [" ".join(all_data[key]['reference']).lower() if type(all_data[key]["reference"]) == list else all_data[key]['reference'] for key, _ in all_data.items()]
 
 # exclude references 
-for key, _ in all_data.items(): 
+for key, _ in predictions_data.items(): 
     if type(all_data[key]["reference"]) == list: 
         reference = " ".join(all_data[key]['reference']) 
     else: 
@@ -27,4 +45,15 @@ for key, _ in all_data.items():
     tagged_reference = get_pos_tags(reference)
 
     if tagged_reference != ['PRP'] and tagged_reference != ['PRP$']: 
+        print(key)
         print(tagged_reference, reference)
+ 
+        filtered_fillers = predictions_data[key]["filtered_fillers"]
+  
+        print(all_data[key]["par"])
+        print(all_data[key]["revised_sentence"])
+        print('\n')
+        print("filtered fillers", filtered_fillers)
+        print("reference", all_data[key]["reference"])
+        print("=========================================")
+
