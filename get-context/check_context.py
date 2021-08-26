2c2e51f81bcc23664e6aac9ec50bd2e1489f2f7d
Talita <talita.anthonio@yahoo.com>
2021-08-26 16:42:58 +0200

    inspect context.

---
+++ get-context/check_context.py
@@ -0,0 +1,24 @@
+import json 
+
+with open("filtered_set_train_articles_tokenized_context.json", "r") as json_in: 
+     data = json.load(json_in)
+
+
+counter = 0 
+for key, _ in data.items(): 
+
+    counter +=1 
+    
+    
+    print(data[key]["BaseArticle"]["left_context"])
+    print(data[key]["BaseArticle"]["current_line"])
+    print(data[key]["BaseArticle"]["right_context"])
+    print("--------new------")
+    print(data[key]["Tokenized_article"]["left"])
+    print(data[key]["Tokenized_article"]["current"])
+    print(data[key]["Tokenized_article"]["right"])
+
+    print("=============================")
+
+    if counter == 30: 
+       break 
